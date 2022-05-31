e370f9e6931abd44230d9facebf6bb21416e018d
Nguyen Van Tinh <tinhnvph20542@fpt.edu.vn>
2022-05-31 19:55:45 +0700

    Tạo các page

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+// vite.config.js
+const { resolve } = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        detailProduct: resolve(__dirname, 'detail.html'),
+        products: resolve(__dirname, 'products.html')
+      },
+    },
+  },
+});
